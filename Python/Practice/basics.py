bf971effa1713bffb2c6d41b41a56ec2bd1272e7
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-20 01:36:55 +0700

    basics Python

---
+++ Python/Practice/basics.py
@@ -0,0 +1,26 @@
+# Printing
+print("Hello World!")
+
+# Variables
+release_year = 2023
+runtime = 120
+rating_out_of_10 = 7.5
+
+# Printing Variables
+print(release_year)
+
+# Calculations
+print(25 * 68 + 13 / 28)
+
+# Changing Variables
+quilt_width = 8
+quilt_length = 12
+
+print( quilt_width * quilt_length)
+
+quilt_length = 8
+
+print(quilt_width * quilt_length)
+
+# Exponents
+print(6 ** 2)
