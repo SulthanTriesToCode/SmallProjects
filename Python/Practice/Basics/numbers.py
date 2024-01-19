b6b3ccb730d8f7a4cb57872878c6b92a065fb107
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-19 18:29:03 +0000

    Jan 20, 2024, 1:29 AM

--- Python/Practice/Basics/numbers.py
+++ Python/Practice/Basics/numbers.py
@@ -1,5 +1,13 @@
+# Printing
+print("Hello World!")
+
+# Variables
 release_year = 2023
 runtime = 120
 rating_out_of_10 = 7.5
 
+# Printing Variables
+print(release_year)
+
+# Calculations
 print(25 * 68 + 13 / 28)
