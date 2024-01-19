7548d7322085ed762729a890043625492ef10ef4
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-19 22:34:24 +0700

    choose your adventure test

---
+++ C#/ChooseYourAdventure/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ChooseYourOwnAdventure
+{
+  class Program
+  {
+      static void Main(string[] args)
+    {
+      /* THE MYSTERIOUS NOISE */
+
+      // Start by asking for the user's name:
+      Console.Write("What is your name?: ");
+      string name = Console.ReadLine();
+      Console.WriteLine($"Hello, {name}! Welcome to our story.");
+      Console.Write("It begins on a cold rainy night. You're sitting in your room and hear a noise coming from down the hall. Do you go investigate?");
+      Console.Write("\n");
+      Console.Write("Type YES or NO: ");
+      string noiseChoice = Console.ReadLine();
+      // turn noisechoice string into uppercase
+      noiseChoice = noiseChoice.ToUpper();
+      Console.WriteLine(noiseChoice);
+      Console.WriteLine("\n");
+    }
+  }
+}
+
+
+
