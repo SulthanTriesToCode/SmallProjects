4dab5726cbcb0c47e7f87ebd21582c2734054c75
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-20 07:39:35 +0000

    Jan 20, 2024, 2:39 PM

--- Python/Practice/basics.py
+++ Python/Practice/basics.py
@@ -81,10 +81,10 @@ if user_name == "Dave":
 # Relational Operators 2
 x = 20
 y = 20
-
 # Write the first if statement here:
 if x == y:
   print("These numbers are the same")
+  
 credits = 120
 
 # Write the second if statement here:
