e31e103d53a955ab1cc991bc6c0aa2c24512f205
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-20 06:38:49 +0000

    Jan 20, 2024, 1:38 PM

--- Python/LovelySeatsReceipt/script.py
+++ Python/LovelySeatsReceipt/script.py
@@ -4,4 +4,6 @@ stylish_settee_description = "Stylish Settee. Faux leather on birch. 29.50 inche
 stylish_settee_price = 180.50
 luxurious_lamp_description = "Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."
 luxurious_lamp_price = 52.15
-sales_
+sales_tax = .088
+
+
