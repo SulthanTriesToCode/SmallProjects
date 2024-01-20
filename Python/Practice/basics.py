36c72d5db1bb01bae1356c58d6dff28da7ce4eba
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-20 03:38:10 +0000

    Jan 20, 2024, 10:38 AM

--- Python/Practice/basics.py
+++ Python/Practice/basics.py
@@ -30,3 +30,14 @@ order_263_r = 263 % 11
 print(order_263_r)
 
 # Concatination
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+# Define message below:
+
+
+#print(message)
