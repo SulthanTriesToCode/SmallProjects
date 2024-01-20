b3cebd65eaa612f8d1aaaf6d76f7e9027d860660
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-20 07:39:20 +0000

    Jan 20, 2024, 2:39 PM

--- Python/Practice/basics.py
+++ Python/Practice/basics.py
@@ -79,4 +79,13 @@ if user_name == "Dave":
   print("Get off my computer Dave!")
 
 # Relational Operators 2
-  
+x = 20
+y = 20
+
+# Write the first if statement here:
+
+
+
+credits = 120
+
+# Write the second if statement here:
