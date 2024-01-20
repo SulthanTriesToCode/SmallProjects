f8dbfc8ca3da8b633f0e35609cb108de6c9da4e5
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-20 03:42:57 +0000

    Jan 20, 2024, 10:42 AM

--- Python/Practice/basics.py
+++ Python/Practice/basics.py
@@ -58,6 +58,8 @@ print("The total price is", total_price)
 to_you = """Stranger, if you passing meet me and desire to speak to me, why
   should you not speak to me?
 And why should I not speak to you?
+"""
 
+print(to_you)
 
 
