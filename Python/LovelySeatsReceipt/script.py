0d03657d8a88668fa1d08f7bb13e93d30465ce46
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-20 06:41:09 +0000

    Jan 20, 2024, 1:41 PM

--- Python/LovelySeatsReceipt/script.py
+++ Python/LovelySeatsReceipt/script.py
@@ -11,7 +11,9 @@ customer_one_itemization = ""
 customer_one_total += lovely_loveseat_price
 customer_one_itemization += lovely_loveseat_description
 customer_one_total += luxurious_lamp_price
-customer
+customer_one_itemization += luxurious_lamp_description
+
+
 
 
 
