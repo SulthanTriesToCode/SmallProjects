fcba1871e4db6527262e73d669a51bb84e905ac6
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-20 16:42:59 +0700

    if statements

--- Python/8ball/magic8.py
+++ Python/8ball/magic8.py
@@ -27,7 +27,14 @@ elif random_number == 9:
 else:
   answer = "Error"
 
-print(name + " asks: " + question)
-print("Magic 8-Ball's answer: " + answer)
+if name == "":
+  print("Question: " + question)
+else:
+  print(name + " asks: " + question)
+
+if question == "":
+  print("The Magic 8-Ball cannot provide a fortune unless you ask it something.")
+else:
+  print("Magic 8-Ball's answer: " + answer)
 
 
