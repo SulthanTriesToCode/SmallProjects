aab9b8f133fbe73dfd33b35a1001f2989a3cb3d5
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-23 12:28:25 +0700

    Python/Practice/Lists:

--- Python/Practice/Lists.py
+++ Python/Practice/Lists.py
@@ -55,4 +55,19 @@ print(last_two_elements)
 slice_off_last_three = suitcase[:-3]
 print(slice_off_last_three)
 
+# Counting in a list
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+jake_votes = votes.count("Jake")
+print(jake_votes)
 
+# Sorting lists I
+addresses = ["221 B Baker St.", "42 Wallaby Way", "12 Grimmauld Place", "742 Evergreen Terrace", "1600 Pennsylvania Ave", "10 Downing St."]
+addresses.sort()
+print(addresses)
+
+# Sorting List II
+games = ["Portal", "Minecraft", "Pacman", "Tetris", "The Sims", "Pokemon"]
+
+games_sorted = sorted(games)
+print(games)
+print(games_sorted)
