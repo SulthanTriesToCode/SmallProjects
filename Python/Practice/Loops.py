54487204ea62b9a01a601c7259f1ea5fbdcc2434
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-23 13:25:33 +0700

    Python/Practice/Loops:

--- Python/Practice/Loops.py
+++ Python/Practice/Loops.py
@@ -25,3 +25,13 @@ while countdown >= 0:
   print(str(countdown))
   countdown -= 1
 print("We have liftoff!")
+
+# WHile Loop with Lists
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+length = len(python_topics)
+index = 0 
+
+while index < length:
+  print("I am learning about " + python_topics[index])
+  index += 1
