0072f1102f460d510b299db3c5202d9fdf2a9e9e
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-23 13:41:10 +0700

    Python/Practice/Loops:

--- Python/Practice/Loops.py
+++ Python/Practice/Loops.py
@@ -47,3 +47,12 @@ for dog_breed in dog_breeds_available_for_adoption:
     print("They have the dog I want!")
     break
 
+# Loop Control: Continue
+
+ages = [12, 38, 34, 26, 21, 19, 67, 41, 17]
+
+for age in ages:
+  if age < 21:
+    continue
+  else: 
+    print(age)
